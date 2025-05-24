e2f64f0475da8e3c3fd632599895efef349d0471
PierreMandel <202198719+MandelPierre@users.noreply.github.com>
2025-05-24 16:25:22 -0300

    criando janela no jogo

---
+++ JogoUno/src/Container.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Container extends JFrame {
+    public Container() {
+        setTitle("UNO");
+        setSize(1024,728);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        this.setResizable(false);
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new Container();
+    }
+}
