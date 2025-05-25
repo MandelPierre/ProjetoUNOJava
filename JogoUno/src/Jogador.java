911f6b9a6053f5f17a3cbd113190fe43fd427483
PierreMandel <202198719+MandelPierre@users.noreply.github.com>
2025-05-25 18:27:27 -0300

    Update Jogador.java

--- JogoUno/src/Jogador.java
+++ JogoUno/src/Jogador.java
@@ -10,4 +10,30 @@ public class Jogador {
         this.mao = new ArrayList<>();
     }
     
+    //Método para comprar uma carta
+    public void comprarCarta(Baralho baralho) {
+        Carta carta = baralho.comprarCarta();
+        if (carta != null) {
+            mao.add(carta);
+            System.out.println(nome + " comprou: " + carta);
+        } else {
+            System.out.println("O baralho esta vazio");
+        }
+    }
+
+    //Método para jogar uma carta da mão
+    public Carta jogarCarta(int indice) {
+        if(indice >= 0 && indice < mao.size()) {
+            return mao.remove(indice);
+        }
+        return null;
+    }
+    
+    //Mostrar a mão do jogador
+    
+    // Verifica se o jogador ficou sem cartas (venceu)
+    
+    // Getter do nome
+
+    // Getter da mão (para lógicas futuras, se quiser)
 }
