ae77417cd5c294020beb3822e0cf237c30dde9d3
PierreMandel <202198719+MandelPierre@users.noreply.github.com>
2025-05-31 16:49:58 -0300

    update carta e cartas coringas coloridas

--- Carta.java
+++ Carta.java
@@ -5,11 +5,13 @@ public class Carta {
     
     private String cor;
     private String valor;
+    private String imagem;
 
     // Construtor: cria uma nova carta com cor e valor
     public Carta(String cor, String valor) {
         this.cor = cor;
         this.valor = valor;
+        this.imagem = "imagens/" + cor + "_" + valor + ".png";
     }
 
     // Getter para obter a cor da carta
@@ -24,12 +26,16 @@ public class Carta {
 
     // Retorna o caminho da imagem da carta
     public String getImagem() {
-        return "imagens/" + cor + "_" + valor + ".png";
+        return imagem;
     }
 
     // Método toString: como a carta será exibida quando imprimir
     @Override
     public String toString() {
         return cor + " " + valor;
+    }
+
+    public void setImagem(String novaImagem) {
+        this.imagem = novaImagem;
     }
 }
